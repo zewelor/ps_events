3bcb4a2d25794cda16c4c857b56972d8d6039b24
Michał Ciemięga <zewelor@gmail.com>
2025-06-06 09:38:31 +0100

    self-host workbox

---
+++ events_listing/assets/js/workbox/workbox-sw.js
@@ -0,0 +1,4 @@
+// Placeholder for Workbox v7.3.0 service worker library.
+// Please download the official file from
+// https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-sw.js
+// and replace this placeholder to enable full functionality.
