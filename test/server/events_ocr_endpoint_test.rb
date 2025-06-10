8d3cb8acbed258b7c53da314772fc0f72f42c5bb
Michał Ciemięga <zewelor@gmail.com>
2025-06-10 09:49:44 +0100

    Enable event OCR to add events

--- test/server/events_ocr_endpoint_test.rb
+++ test/server/events_ocr_endpoint_test.rb
@@ -7,17 +7,32 @@ require_relative "../../bin/server"
 class EventsOcrEndpointTest < Minitest::Test
   include Rack::Test::Methods
 
+  class DummySheets
+    attr_reader :rows
+
+    def initialize
+      @rows = []
+    end
+
+    def append_row(_spreadsheet_id, _range, data)
+      @rows << data
+    end
+  end
+
   def app
     Sinatra::Application
   end
 
   def setup
     @orig_env = app.settings.environment
+    @orig_sheets = app.settings.google_sheets
     app.set :environment, :test
+    app.settings.google_sheets = DummySheets.new
   end
 
   def teardown
     app.set :environment, @orig_env
+    app.settings.google_sheets = @orig_sheets
   end
 
   def test_missing_token
@@ -35,7 +50,7 @@ class EventsOcrEndpointTest < Minitest::Test
   def test_successful_ocr
     mock = Object.new
     def mock.analyze(_path)
-      "csv"
+      '[{"name":"Test","description":"Uma descricao valida","location":"Porto","organizer":"Org","start_date":"01/12/2025","start_time":"","end_date":"02/12/2025","end_time":"","category":"Música","price_type":"Gratuito"}]'
     end
 
     EventOcrService.stub :new, mock do
@@ -50,6 +65,6 @@ class EventsOcrEndpointTest < Minitest::Test
     assert last_response.ok?
     body = JSON.parse(last_response.body)
     assert_equal "ok", body["status"]
-    assert_equal "csv", body["text"]
+    assert_equal 1, app.settings.google_sheets.rows.length
   end
 end
