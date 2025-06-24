093b942edc99b3d6def24f4bf8ee2b45fa285da4
Michał Ciemięga <zewelor@gmail.com>
2025-06-24 09:26:15 +0100

    fix pwa console warnings

--- events_listing/assets/js/pwa-install.js
+++ events_listing/assets/js/pwa-install.js
@@ -5,9 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let deferredPrompt;
 
   window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
+    // Store the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI to notify the user they can add to home screen
     if (installBanner) {
