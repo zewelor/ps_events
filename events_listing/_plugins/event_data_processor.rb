9c3e23d2b64607b136442e184917dec99f094206
zewelor <zewelor@gmail.com>
2025-07-01 13:50:11 +0100

    Include canonical_url in event data

--- events_listing/_plugins/event_data_processor.rb
+++ events_listing/_plugins/event_data_processor.rb
@@ -8,5 +8,6 @@ Jekyll::Hooks.register :site, :post_read do |site|
 
     record["image"] = "/assets/images/#{record["image"]}.webp" if record["image"] && record["image"] != ""
     record["page_slug"] = Class.new.extend(Jekyll::Sanitizer).sanitize_filename(record["start_date"].tr("/", "-") + "-" + record["name"])
+    record["canonical_url"] = "/events/#{record["page_slug"]}"
   end
 end
