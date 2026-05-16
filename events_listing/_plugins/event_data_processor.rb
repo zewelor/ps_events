fcd07e91234ad6eb447e131da94b0cf5fac52143
zewelor <zewelor@gmail.com>
2026-05-16 10:09:55 +0100

    Fixes

--- events_listing/_plugins/event_data_processor.rb
+++ events_listing/_plugins/event_data_processor.rb
@@ -10,7 +10,7 @@ Jekyll::Hooks.register :site, :post_read do |site|
     next unless record.is_a?(Hash)
 
     record["image"] = "/assets/images/#{record["image"]}.webp" if record["image"] && record["image"] != ""
-    record["page_slug"] = Class.new.extend(Jekyll::Sanitizer).sanitize_filename(record["start_date"].tr("/", "-") + "-" + record["name"])
+    record["page_slug"] = Class.new.extend(Jekyll::Sanitizer).sanitize_filename(record["start_date"].tr("/", "-") + "-" + record["name"]).delete_prefix(".").delete_suffix(".")
     record["canonical_url"] = File.join(site.config.fetch("url"), events_dir, record["page_slug"])
   end
 end
